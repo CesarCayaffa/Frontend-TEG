e73f01fd2382abcbc454fda776e96d95ae02814b
CesarCayaffa <cesarcayaffa0@gmail.com>
2024-05-09 15:26:25 -0400

    chore: Update app name to BoviTEGen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'App-TEG',
+  appName: 'BoviTEGen',
   webDir: 'www',
   server: {
     androidScheme: 'https'
