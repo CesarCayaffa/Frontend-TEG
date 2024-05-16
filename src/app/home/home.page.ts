7f7fd4cc68b9b542e4462ec5ea1f49451f7a3607
CesarCayaffa <cesarcayaffa0@gmail.com>
2024-05-16 17:36:07 -0400

    chore: Update home page to include theme service in constructor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +11,14 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
   email: string = '';
-  password: string = ''; 
+  password: string = '';
 
-  constructor(private router: Router, private http: HttpClient, private toastController: ToastController) {}
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private toastController: ToastController,
+    private themeService: ThemeService
+  ) {}
 
   ngOnInit() {
     const token = localStorage.getItem('token');
@@ -25,23 +31,23 @@ export class HomePage implements OnInit {
     const url = 'https://backend-teg.up.railway.app/users/login';
     const body = {
       email: this.email,
-      password: this.password
+      password: this.password,
     };
-  
-    this.http.post(url, body, {responseType: 'text'}).subscribe(
-      data => {
+
+    this.http.post(url, body, { responseType: 'text' }).subscribe(
+      (data) => {
         console.log('Login successful', data);
         localStorage.setItem('token', data);
-        
 
         this.router.navigate(['/lista-vacas']);
       },
-      error => {
-        
-        this.toastController.create({
-          message: 'Error: '+error.error,
-          duration: 2000
-        }).then(toast => toast.present());
+      (error) => {
+        this.toastController
+          .create({
+            message: 'Error: ' + error.error,
+            duration: 2000,
+          })
+          .then((toast) => toast.present());
       }
     );
   }
