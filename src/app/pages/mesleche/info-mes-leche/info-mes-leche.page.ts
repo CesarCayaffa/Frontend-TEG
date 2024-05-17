729f5d45f617379122348dbb39b79b48cedf8344
CesarCayaffa <cesarcayaffa0@gmail.com>
2024-05-17 15:13:30 -0400

    feat: Actualizar página InfoMesLeche para obtener datos del animal en el método ionViewDidEnter

--- src/app/pages/mesleche/info-mes-leche/info-mes-leche.page.ts
+++ src/app/pages/mesleche/info-mes-leche/info-mes-leche.page.ts
@@ -3,15 +3,15 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { ThemeService } from '../../../services/theme.service';
+import { ViewDidEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-info-mes-leche',
   templateUrl: './info-mes-leche.page.html',
   styleUrls: ['./info-mes-leche.page.scss'],
 })
-export class InfoMesLechePage implements OnInit {
+export class InfoMesLechePage implements OnInit, ViewDidEnter{
   private baseUrl = 'https://backend-teg.up.railway.app/animals';
   animalId: string = '';
   animal: any;
@@ -29,51 +29,25 @@ export class InfoMesLechePage implements OnInit {
     const id = this.route.snapshot.paramMap.get('animalId');
     if (id) {
       this.animalId = id;
-      this.http.get(`${this.baseUrl}/${this.animalId}`).subscribe((data) => {
-        this.animal = data;
-        console.log(this.animal);
-      });
     }
   }
 
-  getAnimalById(id: string): Observable<any> {
-    const url = `${this.baseUrl}/${id}`;
-    return this.http.get(url);
+  ionViewDidEnter() {
+    this.http.get(`${this.baseUrl}/${this.animalId}`).subscribe((data) => {
+      this.animal = data;
+    });
   }
+
+  // getAnimalById(id: string): Observable<any> {
+  //   const url = `${this.baseUrl}/${id}`;
+  //   return this.http.get(url);
+  // }
 
   addLecheMes(id: string) {
     this.router.navigate(['/add-mes-leche', this.animalId]);
   }
   updateLecheMes(mesLecheId: string) {
     this.router.navigate(['/update-mes-leche', this.animalId, mesLecheId]);
-  }
-  deleteLecheMes(mesLecheId: string) {
-    this.alertController
-      .create({
-        header: 'Eliminar',
-        message: '¿Estás seguro de que deseas eliminar este mes de leche?',
-        buttons: [
-          {
-            text: 'Cancelar',
-            role: 'cancel',
-          },
-          {
-            text: 'Eliminar',
-            handler: () => {
-              const url = `${this.baseUrl}/deleteLecheMes/${this.animalId}/${mesLecheId}`;
-
-              this.http.patch(url, {}).subscribe(() => {
-                this.getAnimalById(this.animalId).subscribe((animal) => {
-                  this.animal = animal;
-                });
-              });
-            },
-          },
-        ],
-      })
-      .then((alert) => {
-        alert.present();
-      });
   }
 
   retroceder() {
