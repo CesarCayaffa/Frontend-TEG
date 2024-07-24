2b143c4537b3cfaa6a614c56258d9936e4d89925
CesarCayaffa <cesarcayaffa0@gmail.com>
2024-07-25 07:36:31 +0800

    feat: Mejorar estilos de la página de calculadora

--- src/app/pages/calculadora/calculadora.page.ts
+++ src/app/pages/calculadora/calculadora.page.ts
@@ -17,17 +17,13 @@ export class CalculadoraPage implements OnInit {
 
   newAnimal: any;
 
-  constructor(
-    private themeService: ThemeService,
-    private http: HttpClient
-  ) { }
+  constructor(private themeService: ThemeService, private http: HttpClient) {}
 
   ngOnInit() {
-    this.getAnimals().subscribe(data => {
-      this.animals = data; 
+    this.getAnimals().subscribe((data) => {
+      this.animals = data;
     });
   }
-
 
   getAnimals(): Observable<any> {
     const url = 'https://backend-teg.up.railway.app/animals/';
@@ -44,50 +40,51 @@ export class CalculadoraPage implements OnInit {
 
   calculateCross(idMother: string, idFather: string) {
     // Busca los animales correspondientes
-    const mother = this.animals.find(animal => animal._id === idMother);
-    const father = this.animals.find(animal => animal._id === idFather);
-  
+    const mother = this.animals.find((animal) => animal._id === idMother);
+    const father = this.animals.find((animal) => animal._id === idFather);
+
     if (!mother || !father) {
       throw new Error('Mother or father not found');
     }
-  
+
     // Extrae la raza, las condiciones y la puntuación
     let raza;
-  if (mother.raza === father.raza) {
-    raza = mother.raza;
-  } else {
-    raza = `Cruzada (${mother.raza} x ${father.raza})`; // Raza cruzada
-  }
+    if (mother.raza === father.raza) {
+      raza = mother.raza;
+    } else {
+      raza = `Cruzada (${mother.raza} x ${father.raza})`; // Raza cruzada
+    }
     const condicionesMother = mother.condiciones || [];
     const condicionesFather = father.condiciones || [];
     const puntuacionMother = mother.puntuacion || 0;
     const puntuacionFather = father.puntuacion || 0;
-  
+
     // Calcula el promedio de las puntuaciones
     const puntuacion = (puntuacionMother + puntuacionFather) / 2;
-  
+
     // Crea un nuevo animal
     const newAnimal = {
       raza,
       condiciones: [...condicionesMother, ...condicionesFather], // Combina las condiciones de ambos padres
       puntuacion,
-      // Agrega aquí cualquier otra propiedad que necesites
+     
     };
-  
+
     console.log(newAnimal);
     return newAnimal;
-
-
   }
 
   calculate() {
-    this.newAnimal = this.calculateCross(this.selectedMother, this.selectedFather);
+    this.newAnimal = this.calculateCross(
+      this.selectedMother,
+      this.selectedFather
+    );
   }
 
   getConditionsString(): string {
     if (this.newAnimal && this.newAnimal.condiciones) {
       return this.newAnimal.condiciones
-        .filter((condicion: any) => !condicion.curada) 
+        .filter((condicion: any) => !condicion.curada)
         .map((condicion: any) => condicion.nombre)
         .join(', ');
     }
