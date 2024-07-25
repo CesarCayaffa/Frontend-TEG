97dd64d3e3874ce48f2a4f26cd3bf3df1158c209
CesarCayaffa <cesarcayaffa0@gmail.com>
2024-07-25 08:01:51 +0800

    feat: Agregar funcionalidad de notificaciones

--- src/app/pages/notificaciones/notificaciones.page.ts
+++ src/app/pages/notificaciones/notificaciones.page.ts
@@ -7,13 +7,48 @@ import { ThemeService } from '../../services/theme.service';
   styleUrls: ['./notificaciones.page.scss'],
 })
 export class NotificacionesPage implements OnInit {
+  notifications = [
+    {
+      recipient: "ObjectId('60df3eafe8421c13d8b57a7a')",
+      message: 'Vaca 1 se acerca a su fecha de parto.',
+      read: false,
+      dateSent: new Date(),
+      dateRead: null,
+      type: 'reminder',
+      priority: 'high',
+    },
+    {
+      recipient: "ObjectId('60df3eafe8421c13d8b57a7a')",
+      message: 'Recuerda actualizar la leche del mes 4 de la Vaca 5',
+      read: false,
+      dateSent: new Date(),
+      dateRead: null,
+      type: 'reminder',
+      priority: 'high',
+    },
+    {
+      recipient: "ObjectId('60df3eafe8421c13d8b57a7c')",
+      message: 'Vaca 12 produciendo menos leche de lo normal.',
+      read: false,
+      dateSent: new Date(),
+      dateRead: null,
+      type: 'alert',
+      priority: 'low',
+    },
+    {
+      recipient: "ObjectId('60df3eafe8421c13d8b57a7d')",
+      message: 'Toro 2 necesita atención veterinaria.',
+      read: false,
+      dateSent: new Date(),
+      dateRead: null,
+      type: 'reminder',
+      priority: 'high',
+    },
+  ];
 
-  constructor(
-    private themeService: ThemeService
-  ) { }
+  constructor(private themeService: ThemeService) {}
 
   ngOnInit() {
     console.log('NotificacionesPage');
   }
-
 }
